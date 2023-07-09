104c51573e8b1d56b4ae070633e3fd3d5cf6367b
Shreeyash Pandey <shreeyash335@gmail.com>
2023-07-09 22:30:48 +0530

    matmul.py: add newlines b/w results
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- matmul.py
+++ matmul.py
@@ -10,4 +10,4 @@ for dtype in ['float32', 'float64', 'int32', 'int64']:
     x = np.random.normal(size=(1000, 1000)).astype(dtype)
     print(f"Running numpy-{dtype}")
     time_taken = timeit.timeit(lambda: benchmark(dtype), number=1)
-    print(f"Time Taken: {time_taken}")
+    print(f"Time Taken: {time_taken}\n")
